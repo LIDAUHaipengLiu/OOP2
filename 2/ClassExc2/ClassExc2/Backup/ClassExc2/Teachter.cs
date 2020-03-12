0e866fe51a6df959a7c36dfae9a2a0e7c81a5f6a
LIDAUHaipengLiu <55245401+LIDAUHaipengLiu@users.noreply.github.com>
2020-03-11 18:03:10 -0700

    Add files via upload

---
+++ 2/ClassExc2/ClassExc2/Backup/ClassExc2/Teachter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassExc2
+{
+    public class Teachter
+    {
+        #region --声明教师类成员变量
+        public string _name;
+        public int _age;
+        public string _dw;
+        public string _zc;
+        public double _gz;
+        #endregion
+
+
+        #region --声明类的方法--
+        /// <summary>
+        /// 调整工资
+        /// </summary>
+        /// <param name="addgz">增加的额度</param>
+        /// <returns>返回调整后的工资额度</returns>
+        public double Tzgz(double addgz)
+        {
+            _gz += addgz;
+            return _gz;
+        }
+
+        public string ZhiCheng(string newzc)
+        {
+            string str="";
+            str = _name + "当前职称是：" + _zc + ",调整为：" + newzc;
+            return str;
+        }
+        #endregion
+
+    }
+}
